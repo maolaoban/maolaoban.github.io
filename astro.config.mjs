69380f7760ec70f437adca9e1fe5fcd66a1d1bd1
pengchao <1393867317@qq.com>
2025-02-20 16:42:29 +0800

    修改astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,6 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   site: "https://maolaoban.github.io",
-  base: "/my-blog",
   integrations: [
     sitemap(),
     robotsTxt({
